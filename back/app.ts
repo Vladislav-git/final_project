9f24ea9a9d814bcf0e12bef6fb735cac8f92e6c7
Vladislav-git <gor.vl.rusl@gmail.com>
2021-04-19 10:49:01 +0300

    add logging to graphql

--- back/app.ts
+++ back/app.ts
@@ -8,10 +8,21 @@ const socketio = require('socket.io')
 const bodyParser = require('body-parser');
 import {Query, Mutation} from './src/resolvers/resolvers'
 import {logger} from './logger'
+const { print } = require('graphql');
 
+class BasicLogging {
+  requestDidStart({queryString, parsedQuery, variables}:any) {
+    const query = queryString || print(parsedQuery);
+    console.log(query);
+    console.log(variables);
+  }
+
+  willSendResponse({graphqlResponse}:any) {
+    console.log(JSON.stringify(graphqlResponse, null, 2));
+  }
+}
 
 require('dotenv').config()
-
 
 const app = express();
 const server = http.createServer(app)
@@ -21,18 +32,22 @@ const PORT = 8000;
 const typeDefs = gql(fs.readFileSync('./src/schemas/schemas.graphql', {encoding: 'utf8'}))
 const resolvers = {Query, Mutation}
 
-const apolloServer = new ApolloServer({typeDefs, resolvers})
+
+const apolloServer = new ApolloServer({typeDefs, resolvers, extensions: [() => new BasicLogging()]})
 apolloServer.applyMiddleware({app, path: '/graphql'})
 Socket(io)
 
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use('/api', apiRouter);
+server.listen(PORT, () => {
+  console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
+});
 
 app.use((req, res, next) => {
   logger.log('info', `Requesting ${req.method} ${req.originalUrl}`, {tags: 'http', additionalInfo: {body: req.body, headers: req.headers }});
   next()      
 })
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use('/api', apiRouter);
-server.listen(PORT, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
-});
--- back/logger.ts
+++ back/logger.ts
@@ -9,14 +9,13 @@ export const logger = new winston.createLogger({
         })
    ]
 });
-if (process.env.NODE_ENV === 'production') {
+
 const cloudwatchConfig = {
     logGroupName: process.env.CLOUDWATCH_GROUP_NAME,
-    logStreamName: `${process.env.CLOUDWATCH_GROUP_NAME}-${process.env.NODE_ENV}`,
+    logStreamName: process.env.CLOUDWATCH_GROUP_NAME,
     awsAccessKeyId: process.env.CLOUDWATCH_ACCESS_KEY,
     awsSecretKey: process.env.CLOUDWATCH_SECRET_ACCESS_KEY,
     awsRegion: process.env.CLOUDWATCH_REGION,
     messageFormatter: ({ level, message, additionalInfo }:any) =>    `[${level}] : ${message} \nAdditional Info: ${JSON.stringify(additionalInfo)}}`
 }
     logger.add(new WinstonCloudWatch(cloudwatchConfig))
-}
